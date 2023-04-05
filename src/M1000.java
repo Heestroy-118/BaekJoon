2c57f50b441877fe2fbde1da80d9d7861ad980e2
Heestroy-118 <a11308@naver.com>
2023-04-05 17:55:20 +0900

    commit

---
+++ src/M1000.java
@@ -0,0 +1,10 @@
+
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class M1000 {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+}
