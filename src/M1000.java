862ec2c729072a534053412dbf74d65d47becb4e
Heestroy-118 <a11308@naver.com>
2023-04-05 18:03:20 +0900

    commit

--- src/M1000.java
+++ src/M1000.java
@@ -6,5 +6,13 @@ import java.io.OutputStreamWriter;
 
 public class M1000 {
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    String s = bf.readLine(); /* 리턴값을 String으로 고정하기 때문에 String이 아닌
+    다른 타입으로 입력을 받으려면 형변환이 반드시 필요하다 */
+    int i = Integer.parseInt(bf.readLine()); //int
+
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String s = "abcdefg";
+
+
 
 }
