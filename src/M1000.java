a6aa55dd0199b40a46a4095e9a88f0d39688f745
Heestroy-118 <a11308@naver.com>
2023-04-06 23:03:08 +0900

    commit

--- src/M1000.java
+++ src/M1000.java
@@ -8,7 +8,7 @@ public class M1000 {
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     String s = bf.readLine(); /* 리턴값을 String으로 고정하기 때문에 String이 아닌
     다른 타입으로 입력을 받으려면 형변환이 반드시 필요하다 */
-    int i = Integer.parseInt(bf.readLine()); //int
+    int i = Integer.parseInt(bf.readLine()); //int 사용
 
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     String s = "abcdefg";
