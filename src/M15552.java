3f0412ae9dc8755c71a081fe21b06aea6f145ee7
Heestroy-118 <a11308@naver.com>
2023-04-07 14:22:24 +0900

    commit

--- src/M15552.java
+++ src/M15552.java
@@ -1,2 +1,12 @@
+// M15552, 첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다.
+// 다음 T줄에는 각각 두 정수 A와 B가 주어진다. A와 B는 1 이상, 1,000 이하이다.
+
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 public class M15552 {
+    public void main
+    BufferedWriter bw = new BufferedWriter(new InputStreamReader(System.in));
+
 }
